bc0cb77a46a92130a7be46bf4ae1759ff899d20f
Osian001 <techstechs7073@gmail.com>
2025-07-25 21:21:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "20767676"
 api_hash = "7e1fd1a8ec48ad69175548350eae87b2"
-bot_token = "8011811111:AAFY6ICb3KMSclHeTlVBgILQyGUSFrictKs"
-sudo_groups = -1002751894880
+bot_token = "7780206214:AAGIdCbGo56-_3acSu_GgXeC06SttAQ_1e0"
+sudo_groups = -1002476320663
